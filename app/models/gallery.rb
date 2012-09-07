a4b74ea6f61634f69a3c34f971dd3224c7cb917c
prabhu <prabhu@ubuntu.ubuntu-domain>
2012-09-07 23:45:37 +0530

    prod

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,4 +3,6 @@ class Gallery < ActiveRecord::Base
   has_many :photos, :as => :imageable
   accepts_nested_attributes_for :photos
   attr_accessible :name, :photos_attributes, :user_id
+  validates :name, :presence => true,:uniqueness => true
+
 end
